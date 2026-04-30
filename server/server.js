597ad369be9d6a1afa569cc0045f9dd760e7be60
[aruntajpuriya1] <[aruntajpuriya1@gmail.com]>
2026-04-30 22:15:57 +0545

    fix(CORS):cors issued fixed:

--- server/server.js
+++ server/server.js
@@ -24,8 +24,10 @@ app.use(helmet());
 // }));
 const allowedOrigins = [
   'https://soniya-beauty-saloon.netlify.app',
+   'http://localhost:5173',
+  'http://127.0.0.1:5173',
   'http://localhost:3000',
-  'http://127.0.0.1:3000'
+  'http://127.0.0.1:3000',
 ];
 
 const corsOptions = {
